687f0836301f68fb755e5e2fcdc8db68f952779c
manpreet singh <manpreet.singh10196@gmail.com>
2019-08-08 15:59:19 +0530

    create controller

--- js/app.js
+++ js/app.js
@@ -2,7 +2,33 @@
  //Budget controller
  var budgetController = (function() {
  	
+ 	var Expense= function(id,description,value){
+        this.id=id;
+        this.description=description;
+        this.value=value;
+ 	};
+    
+    var Income= function(id,description,value){
+        this.id=id;
+        this.description=description;
+        this.value=value;
+ 	};
+
+    var data ={
+    	allitems:{
+    		exp:[],
+    		inc:[]
+    	},
+    	totals:{
+    		exp:0,
+    		inc:0
+    	}
+    }
+
   })(); 
+
+
+
 
 //UI controller
  var UIController =(function(){
